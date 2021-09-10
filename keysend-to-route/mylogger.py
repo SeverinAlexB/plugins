7ede964555d40dc3fc85c15baee630770c388bc5
Severin Buhler <sevi_buehler@hotmail.com>
2021-09-10 15:16:27 +0300

    logger to dir

--- keysend-to-route/mylogger.py
+++ keysend-to-route/mylogger.py
@@ -1,9 +1,11 @@
 import logging
 
+import os
+
 def get_my_logger():
     logger = logging.getLogger('keysend-to-route')
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler('keysend-to-route.log')
+    fh = logging.FileHandler(os.path.dirname(os.path.abspath(__file__)) + '/keysend-to-route.log')
     fh.setLevel('DEBUG')
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
