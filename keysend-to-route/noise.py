cceebcac57d06485b90f06b3d79c04c0c93fbd91
Severin Buhler <sevi_buehler@hotmail.com>
2021-09-10 13:36:53 +0300

    added first draft of keysend-to-route

---
+++ keysend-to-route/noise.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+from binascii import hexlify
+from onion import OnionPayload
+from onion import TlvPayload
+from pyln.client import Plugin, RpcError
+import hashlib
+import os
+import struct
+import time
+import zbase32
+
+
+plugin = Plugin()
+
+TLV_KEYSEND_PREIMAGE = 5482373484
+TLV_NOISE_MESSAGE = 34349334
+TLV_NOISE_SIGNATURE = 34349335
+TLV_NOISE_TIMESTAMP = 34349343
+
+
+class Message(object):
+    def __init__(self, sender, body, signature, payment=None, id=None):
+        self.id = id
+        self.sender = sender
+        self.body = body
+        self.signature = signature
+        self.payment = payment
+        self.verified = None
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "sender": self.sender,
+            "body": self.body,
+            "signature": hexlify(self.signature).decode('ASCII'),
+            "payment": self.payment.to_dict() if self.payment is not None else None,
+            "verified": self.verified,
+        }
+
+
+class Payment(object):
+    def __init__(self, payment_key, amount):
+        self.payment_key = payment_key
+        self.amount = amount
+
+    def to_dict(self):
+        return {
+            "payment_key": hexlify(self.payment_key).decode('ASCII'),
+            "payment_hash": hashlib.sha256(self.payment_key).hexdigest(),
+            "amount": self.amount,
+        }
+
+
+def serialize_payload(n, blockheight):
+    block, tx, out = n['channel'].split('x')
+    payload = hexlify(struct.pack(
+        "!cQQL", b'\x00',
+        int(block) << 40 | int(tx) << 16 | int(out),
+        int(n['amount_msat']),
+        blockheight + n['delay'])).decode('ASCII')
+    payload += "00" * 12
+    return payload
+
+
+def buildpath(plugin, payload, route):
+    blockheight = plugin.rpc.getinfo()['blockheight']
+    first_hop = route[0]
+    # Need to shift the parameters by one hop
+    hops = []
+    for h, n in zip(route[:-1], route[1:]):
+        # We tell the node h about the parameters to use for n (a.k.a. h + 1)
+        hops.append({
+            "type": "legacy",
+            "pubkey": h['id'],
+            "payload": serialize_payload(n, blockheight)
+        })
+
+    # The last hop has a special payload:
+    hops.append({
+        "type": "tlv",
+        "pubkey": route[-1]['id'],
+        "payload": hexlify(payload).decode('ASCII'),
+    })
+    return first_hop, hops, route
+
+
+def deliver(node_id, payload, payment_hash, route):
+    """Do your best to deliver `payload` to `node_id`.
+    """
+    payment_hash = hexlify(payment_hash).decode('ASCII')
+
+    plugin.log("Starting attempt {} to deliver message to {}".format(node_id))
+
+    first_hop, hops, route = buildpath(plugin, payload, route)
+    onion = plugin.rpc.createonion(hops=hops, assocdata=payment_hash)
+
+    plugin.rpc.sendonion(onion=onion['onion'],
+                         first_hop=first_hop,
+                         payment_hash=payment_hash,
+                         shared_secrets=onion['shared_secrets']
+                         )
+    try:
+        plugin.rpc.waitsendpay(payment_hash=payment_hash)
+        return {'route': route, 'payment_hash': payment_hash, 'success': True}
+    except RpcError as e:
+        failcode = e.error['data']['failcode']
+        failingidx = e.error['data']['erring_index']
+        if failcode == 16399 or failingidx == len(hops):
+            return {'route': route, 'payment_hash': payment_hash, 'success': False}
+
+    raise ValueError('Could not reach destination {node_id}'.format(node_id=node_id))
+
+
+@plugin.async_method('keysend-to-route')
+def keysend_to_route(node_id, route, request, **kwargs):
+    payload = TlvPayload()
+
+    payment_key = os.urandom(32)
+    payment_hash = hashlib.sha256(payment_key).digest()
+
+    payload.add_field(TLV_KEYSEND_PREIMAGE, payment_key)
+
+
+    res = deliver(
+        node_id,
+        payload.to_bytes(),
+        route=route,
+        payment_hash=payment_hash
+    )
+    request.set_result(res)
+
+
+@plugin.init()
+def init(configuration, options, plugin, **kwargs):
+    print("Starting keysend-to-route plugin")
+
+
+plugin.run()
---
+++ keysend-to-route/test_chat.py
@@ -0,0 +1,103 @@
+from onion import TlvPayload
+from flaky import flaky
+from pprint import pprint
+from pyln.client import RpcError
+from pyln.testing.fixtures import *  # noqa: F401,F403
+
+import hashlib
+import os
+import pytest
+import unittest
+import zbase32
+
+
+plugin = os.path.join(os.path.dirname(__file__), 'noise.py')
+
+
+def test_sendmsg_success(node_factory, executor):
+    opts = [{'plugin': plugin}, {}, {'plugin': plugin}]
+    l1, l2, l3 = node_factory.line_graph(3, wait_for_announce=True, opts=opts)
+
+    recv = executor.submit(l3.rpc.recvmsg)
+    l1.rpc.sendmsg(l3.info['id'], "Hello world!")
+
+    # This one is tailing the incoming messages
+    m1 = recv.result(10)
+
+    # This one should get the same result:
+    m2 = l3.rpc.recvmsg(last_id=-1)
+    # They should be the same :-)
+    assert(m1 == m2)
+
+    assert(m2['sender'] == l1.info['id'])
+    assert(m2['verified'] is True)
+
+
+
+def test_zbase32():
+    zb32 = b'd75qtmgijm79rpooshmgzjwji9gj7dsdat8remuskyjp9oq1ugkaoj6orbxzhuo4njtyh96e3aq84p1tiuz77nchgxa1s4ka4carnbiy'
+    b = zbase32.decode(zb32)
+    enc = zbase32.encode(b)
+    assert(enc == zb32)
+
+
+
+
+
+def test_forward_ok(node_factory, executor):
+    """All nodes run plugin, forwarding node doesn't crash.
+
+    Reproduces the crash mentioned by @darosior in this comment:
+    https://github.com/lightningd/plugins/pull/68#issuecomment-577251902
+
+    """
+    opts = [{'plugin': plugin}] * 3
+    l1, l2, l3 = node_factory.line_graph(3, wait_for_announce=True, opts=opts)
+
+    recv = executor.submit(l3.rpc.recvmsg)
+    l1.rpc.sendmsg(l3.info['id'], "Hello world!")
+
+    # This one is tailing the incoming messages
+    m1 = recv.result(10)
+
+    # This one should get the same result:
+    m2 = l3.rpc.recvmsg(last_id=-1)
+    # They should be the same :-)
+    assert(m1 == m2)
+
+    assert(m2['sender'] == l1.info['id'])
+    assert(m2['verified'] is True)
+
+
+def test_missing_tlv_fields(node_factory):
+    """If we're missing a field we should not crash
+    """
+    opts = [{'plugin': plugin}] * 2
+    l1, l2 = node_factory.line_graph(2, wait_for_announce=True, opts=opts)
+    payment_key = os.urandom(32)
+    payment_hash = hashlib.sha256(payment_key).hexdigest()
+
+    route = l1.rpc.getroute(l2.info['id'], 10, 10)['route']
+
+    def send(key, value):
+        hops = [{"type": "tlv", "pubkey": l2.info['id'], "payload": None}]
+
+        payload = TlvPayload()
+        payload.add_field(key, value)
+
+        hops[0]['payload'] = payload.to_hex()
+        onion = l1.rpc.createonion(hops=hops, assocdata=payment_hash)
+        l1.rpc.sendonion(
+            onion=onion['onion'],
+            first_hop=route[0],
+            payment_hash=payment_hash,
+            shared_secrets=onion['shared_secrets'],
+        )
+        with pytest.raises(RpcError, match=r'WIRE_INVALID_ONION_PAYLOAD'):
+            l1.rpc.waitsendpay(payment_hash)
+
+    send(34349334, b'Message body')
+    assert(l2.daemon.wait_for_log(r'Missing message body or signature'))
+
+    send(34349335, b'00' * 32)
+    assert(l2.daemon.wait_for_log(r'Missing message body or signature'))
