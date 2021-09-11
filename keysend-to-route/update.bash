d414ab5fefade49857d859a96a9adbf110c7a5ee
Severin Buhler <sevi_buehler@hotmail.com>
2021-09-11 13:43:07 +0300

    updated things

--- keysend-to-route/update.bash
+++ keysend-to-route/update.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git pull
-/home/bitcoin/lightning/cli/lightning-cli plugin stop /home/bitcoin/keysend-to-route/keysend-to-route/keysend-to-route.py
-/home/bitcoin/lightning/cli/lightning-cli plugin start /home/bitcoin/keysend-to-route/keysend-to-route/keysend-to-route.py
+/home/bitcoin/lightning/cli/lightning-cli plugin stop /home/bitcoin/keysend-to-route/keysend-to-route/keysend_to_route.py
+/home/bitcoin/lightning/cli/lightning-cli plugin start /home/bitcoin/keysend-to-route/keysend-to-route/keysend_to_route.py
